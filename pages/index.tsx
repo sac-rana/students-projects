5eef908739262b90e994fec5af3a4e102404776a
Sachin Rana <sachinrana1827@gmail.com>
2022-03-07 20:44:23 +0530

    shorten array syntax

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  const arr = Array.from<number, number>({ length: 10 }, (ele, i) => i);
+  const arr = [...new Array(10).keys()];
   return (
     <div>
       {arr.map(a => (
         <div key={a}>
           <h2>Some project name</h2>
+          <p>College name</p>
           <p>some description</p>
         </div>
       ))}
